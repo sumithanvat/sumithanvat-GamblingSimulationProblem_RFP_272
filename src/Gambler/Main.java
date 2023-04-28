db6498546bbd17a22b72b681f97a49370f5e8508
sumithanvat <skhanvat@gmail.com>
2023-04-29 00:26:32 +0530

    Added:As a Gambler make  bet so either win or loose

--- src/Gambler/Main.java
+++ src/Gambler/Main.java
@@ -1,9 +1,26 @@
 package Gambler;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        Gambler gambler = new Gambler(100, 1);
-        gambler.play();
+        int stake = 100; // Starting stake
+        int bet = 1; // Betting $1 per game
+        Random random = new Random(); // Random number generator
+
+        while (stake > 0) {
+            // Roll the dice
+            int outcome = random.nextInt(2);
+
+            if (outcome == 0) { // The gambler lost the game
+                stake -= bet;
+            } else { // The gambler won the game
+                stake += bet;
+            }
+        }
+
+        System.out.println("You are broke!");
+
     }
 }
